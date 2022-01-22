6ec4060fbb834435eecc0bc2a4a81c0069dc2168
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-22 13:26:40 +0530

    Node File

---
+++ Node.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day14_LinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+
+        }
+    }
+}
