0ae7a48a37d63e2fd583a561048f2ead8aa895a5
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-22 13:03:38 +0530

    uc4

---
+++ uc4_insertionInLinkedList.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day10_LinkedList
+{
+    class uc4_insertionInLinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void AddFirst(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                node.next = temp;
+                head = temp;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void AddLast(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public int Length()
+        {
+            Node temp;
+            int Count = 0;
+            temp = this.head;
+            while (temp != null)
+            {
+                Count++;
+                temp = temp.next;
+            }
+            return Count;
+        }
+
+        public void AddAfter(int Ploc, int data)
+        {
+            Node temp;
+            Node p;
+            int loc = Ploc, i = 1;
+            int leng = this.Length();
+            if (loc > leng)
+            {
+                Console.WriteLine("Invalid Location");
+                Console.WriteLine("Current list having ", leng);
+            }
+            else
+            {
+                p = this.head;
+                while (i < loc)
+                {
+                    p = p.next;
+                    i++;
+                }
+                Node node = new Node(data);
+                temp = node;
+                temp.next = p.next;
+                Console.WriteLine("{0} inserted into after {1} linked list", node.data, p.data);
+                p.next = temp;
+            }
+        }
+
+    }
+}
